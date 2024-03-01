21f5fc03d21b232f2b5539dd4e73606eac301224
Carlos Alberto Schnaider <schcarlos18@gmail.com>
2024-03-01 15:07:09 -0300

    Adicionado usage()

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Bem vindo ao Python")
+def usage():
+    print ("###########################################################################################")
+    print ("#  Objective:  remove dbo. and table|StoredProcedure|trigger|function|view from filename  #")
+    print ("#                                                                                         #")
+    print ("#  Usage:  fix_sqlfiles <table | procedures | trigger | view>                             #")
+    print ("###########################################################################################")
+    
+
+if __name__ == "__main__":
+    usage()
